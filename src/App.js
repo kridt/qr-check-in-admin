b06e7460edec4543095a5a22334bf0e3a796ff96
Christian Nielsen <chrnielsen2003@gmail.com>
2024-01-18 02:58:51 +0100

    Add complete_medarbejder_data.json and update LandingPage.js

--- src/App.js
+++ src/App.js
@@ -7,12 +7,35 @@ import AddLocation from "./pages/AddLocation";
 import Start from "./pages/Start";
 import { CoworkerIdContext } from "./contexts/coworkerContext";
 import PrintQr from "./pages/PrintQr";
+import axios from "axios";
+import { database } from "./firebase";
 
 function App() {
   const [coworkerId, setCoworkerId] = useState(false);
 
+  /* function test() {
+    axios.get("/complete_medarbejder_data.json").then((res) => {
+      res.data.forEach((coworker) => {
+        const docId = `${
+          "nr:" +
+          coworker.id +
+          "-" +
+          coworker.first_name.replaceAll(" ", "-") +
+          "-" +
+          coworker.last_name.replaceAll(" ", "-")
+        }`;
+        console.log(docId);
+        database.collection("coworkers").doc(docId).set({
+          firstName: coworker.first_name,
+          lastName: coworker.last_name,
+          id: coworker.id,
+        });
+      });
+    });
+  } */
   return (
     <CoworkerIdContext.Provider value={{ coworkerId, setCoworkerId }}>
+      {/* <button onClick={() => test()}>sefsfe</button> */}
       <BrowserRouter className="App">
         <Routes>
           <Route path="/" element={<Start />} />
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,15 +1,111 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./LandingPage.css";
 import { useParams } from "react-router-dom";
+import { database } from "../firebase";
+import axios from "axios";
 
 export default function LandingPage() {
   const checkId = useParams().check;
   const locationId = useParams().locationId;
-  console.log(locationId, checkId);
+  const [currentLocation, setCurrentLocation] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [allCoworkers, setAllCoworkers] = useState([]);
+
+  useEffect(() => {
+    var allLocations = [];
+
+    database
+      .collection("locations")
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          //console.log(doc.id, " => ", doc.data());
+          allLocations.push(doc.data());
+        });
+      })
+      .then(() => {
+        const currentLocation = allLocations?.find(
+          (location) => location.locationId === parseInt(locationId)
+        );
+
+        setCurrentLocation(currentLocation);
+        setLoading(false);
+      });
+
+    database
+      .collection("coworkers")
+      .get()
+      .then((querySnapshot) => {
+        setAllCoworkers(querySnapshot.docs.map((doc) => doc.data()));
+      })
+      .catch((error) => {
+        console.log("Error getting documents: ", error);
+      });
+  }, [checkId, locationId]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const number = e.target.numberInput.value;
+    const findNumer = number?.match(/\d+/) || 0;
+
+    const coworker = allCoworkers?.find(
+      (e) => e?.id === parseInt(findNumer[0] || 0)
+    );
+
+    if (!coworker) {
+      alert("Coworker number does not exist");
+      return;
+    }
+
+    if (
+      window.confirm(
+        `Check ${checkId}` +
+          " " +
+          coworker.firstName +
+          " " +
+          coworker.lastName +
+          " at " +
+          currentLocation?.locationAddress +
+          "?"
+      )
+    ) {
+      /* console.log(
+        "Checked in " +
+          coworker.firstName +
+          " " +
+          coworker.lastName +
+          " at " +
+          currentLocation?.locationAddress +
+          " at " +
+          new Date().toLocaleString()
+      ); */
+
+      axios
+        .get("http://localhost:6969/api/start", {
+          params: {
+            checkId: checkId,
+            firstName: coworker.firstName,
+            lastName: coworker.lastName,
+            coworkerId: coworker.id,
+            locationAddress: currentLocation?.locationAddress,
+            time: new Date().toLocaleString(),
+            locationId: currentLocation?.locationId,
+          },
+        })
+        .then((res) => console.log(res.data));
+    }
+  }
 
   return (
     <div className="landing-page">
-      <form className="form">
+      {loading ? (
+        <h2 style={{ textAlign: "center" }}>Finding location...</h2>
+      ) : (
+        <h2 style={{ textAlign: "center" }}>
+          Check {checkId} at {currentLocation?.locationAddress}
+        </h2>
+      )}
+      <form onSubmit={(e) => handleSubmit(e)} className="form">
         <label htmlFor="numberInput">Enter your company number:</label>
         <input type="tel" id="numberInput" />
         <button type="submit">Check {checkId}</button>
